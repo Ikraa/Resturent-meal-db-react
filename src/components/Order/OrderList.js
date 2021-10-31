c9d4d8d61b02df4dc91013bb46d669bbb0f875aa
Your Name <you@example.com>
2021-10-31 16:11:07 +0600

    added local storage quantity

--- src/components/Order/OrderList.js
+++ src/components/Order/OrderList.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './OrderList.css';
 
 const OrderList = (props) => {
-    const {order} = props;
+    const { order } = props;
+    const countReducer = (previos, meal) => previos+meal.quantity;
+    const count= order.reduce (countReducer, 0);
+
+
     return (
         <div>
             <h2>Order List</h2>
-            <h4>Items ordered:{order.length}</h4>
+            {/* <h4>Items ordered:{order.length}</h4> */}
+            <h4>Items ordered: {count}</h4>
         </div>
     );
 };
--- src/components/Resturent/Resturent.js
+++ src/components/Resturent/Resturent.js
@@ -3,8 +3,8 @@ import Meal from '../Meal/Meal';
 import './Resturent.css';
 import OrderList from '../Order/OrderList';
 import { addToDb, getDb } from '../../localStorage/LocalStorage';
-const Resturent = () => {
 
+const Resturent = () => {
     const [meals, setMeals] = useState([]);
     const [order, setOrder] = useState([]);
 
@@ -12,53 +12,58 @@ const Resturent = () => {
         fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
             .then(res => res.json())
             .then(data => setMeals(data.meals));
-
     }, []);
-
+    /* 
+        The above api link or the below method will now work for search. 
+        if you want to implement search in this code. 
+        1. add a input field 
+        2. declare a state to keep search field text
+        3. Make meal loading api to dependant on search text
+        4. change the meal loading api.you will get the right api link on their website.
+        5. make the meal loading api dynamic using template string. 
+        6. Also, the useEffect below will not work. Because, search result might not include the meals previously added to the cart
+        7. in that case, for each mealId, you have to load the meal from the api (you will find a new pai to load meal by Id) and then add them to the order state.
+        ---------------  
+        Read carefully, give it a try. [ Ki ache jibone]
+        if  you need help, let us know in the support session
+    */
     useEffect(() => {
-
-        console.log("call local Storage");
-        if (meals.length){
+        if (meals.length) {
             const savedDb = getDb();
-            const savedOrder= [];
+            const savedOrder = [];
             for (const mealId in savedDb) {
-                const meal = meals.find(ml => ml.idMeal === mealId);
+                const meal = meals.find(ml => ml.idMeal === mealId)
+                const quantity = savedDb[mealId];
+                meal.quantity = quantity;
                 savedOrder.push(meal);
             }
             setOrder(savedOrder);
         }
-
-    }, [meals]);
+    }, [meals])
 
     const handleAddToOrder = meal => {
+        meal.quantity=1;
         const newOrder = [...order, meal];
         setOrder(newOrder);
-        console.log(meal);
         addToDb(meal.idMeal);
     }
 
-
     return (
-        <div className='resturent-menu'>
-            <div className='meals-container'>
+        <div className="resturent-menu">
+            <div className="meals-container">
                 {
                     meals.map(meal => <Meal
                         key={meal.idMeal}
                         meal={meal}
                         handleAddToOrder={handleAddToOrder}
-                    >
-
-                    </Meal>)
+                    ></Meal>)
                 }
-
             </div>
-            <div className='order-list'>
-
+            <div className="order-list">
                 <OrderList order={order}></OrderList>
-
             </div>
-
         </div>
+
     );
 };
 
